351c55ee4a8cbcefaeb8d81a9ac356d019e472f0
Nawel Bellil <153316848+Nawel-Bellil@users.noreply.github.com>
2024-10-18 14:49:50 +0100

    user model

---
+++ backend/models/user.js
@@ -0,0 +1,21 @@
+// models/User.js
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: {
+      type: String,
+      enum: ["manager", "operator", "operatorSupervisor"],
+      required: true,
+    },
+    assignedMachines: [{ type: Schema.Types.ObjectId, ref: "Machine" }], // Operators and Supervisors may have assigned machines
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
