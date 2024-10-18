c4b5353e8ceae9aa43ed5aa52f03e55c6485b6b3
Nawel Bellil <153316848+Nawel-Bellil@users.noreply.github.com>
2024-10-18 22:32:03 +0100

    setup for controllers/routes

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express = require("express"); 
 const cors = require("cors"); 
 const connectToDB = require("./lib/connectToDB");
-
 const app = express(); 
 
 // Middleware
