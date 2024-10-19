679ea599a7f5b1b1ea44ce5d469c19bf91ab6c9e
Nawel Bellil <153316848+Nawel-Bellil@users.noreply.github.com>
2024-10-19 22:25:32 +0100

    cron update

--- backend/cron/taskScheduler.js
+++ backend/cron/taskScheduler.js
@@ -1,8 +1,8 @@
 const cron = require('node-cron');
 const { scheduleTasks } = require('../services/taskService');
 
-// Schedule task every hour (you can change the interval as needed)
-cron.schedule('0 * * * *', async () => {
+// Schedule task every 6 hours
+cron.schedule('0 */6 * * *', async () => {
     try {
         await scheduleTasks();
         console.log('Tasks scheduled successfully.');
